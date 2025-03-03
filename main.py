0cc846ddc0c95a9c502a9640f09a77b44f6b2594
Reynaldo Garcia <garciar17@southernct.edu>
2025-03-02 19:07:47 -0500

    Converted code in main.py to a function.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
+# addNum function that adds the user-inputted numbers to a ruuning total
+def addNum ():
+    total = 0
+    for i in range(5):
+        number = input("Enter a number: ")
+        total += int(number)
+    return total
 
-total = 0
+# Main function that calls the addNum function
+def main():
+    print("The running total is: ",  addNum())
 
-for i in range(5):
-    number = input("Enter a number: ")
-    total += int(number)
-
-print("The running total is: ", + total)
+if __name__ == "__main__":
+    main()
